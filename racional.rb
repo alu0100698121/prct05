b6904a5b61cca3abcdebabf0a145c5637a459704
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-17 19:03:17 +0100

    FINALIZADO: Creada clase y pruebas unitarias

--- racional.rb
+++ racional.rb
@@ -3,4 +3,63 @@
 require "./gcd.rb"
 
 class Fraccion
+	attr_accessor :n, :d #Getters & Setters
+	
+	def initialize (n, d) #Inicializa el numerador (n) y denominador (d) de la fraccion.
+		if (d != 0)
+			@n, @d = n, d
+		else
+			@n, @d = n, 1
+		end
+	end
+	
+	def to_s #Devuelve una cadena con la representacion del racional
+		"#{@n}/#{@d}"
+	end
+	
+	def suma (other) #Devuelve un nuevo racional que suma al objeto que invoca el que le pasan como parametro
+		if @d != other.d #Si los denominadores son distintos
+			mcm = (@d * other.d) / gcd(@d, other.d) #Se halla el minimo comun multiplo
+			num_a = (mcm / @d) * @n					#Se halla el nuevo numerador del primer sumando
+			num_b = (mcm / other.d) * other.n		#Se halla el nuevo numerador del segundo sumando
+		else	#Si los denominadores son iguales
+			mcm = @d			#Se mantiene el divisor
+			num_a = @n			
+			num_b = other.n
+		end
+		
+		num_res = num_a+num_b					#Se halla el numerador del resultado de la suma
+		mcd = gcd(num_res,mcm)					#Se halla el maximo comun divisor entre numerador y denominador
+												#para reducirlos a la minima expresion
+		Fraccion.new(num_res/mcd, mcm/mcd)
+	end
+	
+	def resta (other) #Devuelve un nuevo racional que resta al objeto que invoca el que le pasan como parametro
+		if @d != other.d #Si los denominadores son distintos
+			mcm = (@d * other.d) / gcd(@d, other.d) #Se halla el minimo comun multiplo
+			num_a = (mcm / @d) * @n					#Se halla el nuevo numerador del minuendo
+			num_b = (mcm / other.d) * other.n		#Se halla el nuevo numerador del sustraendo
+		else	#Si los denominadores son iguales
+			mcm = @d			#Se mantiene el divisor
+			num_a = @n			
+			num_b = other.n
+		end
+		
+		num_res = num_a-num_b					#Se halla el numerador del resultado de la resta
+		mcd = gcd(num_res,mcm)					#Se halla el maximo comun divisor entre numerador y denominador
+												#para reducirlos a la minima expresion
+		Fraccion.new(num_res/mcd, mcm/mcd)
+	end
+	
+	def producto (other) #Devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parametro
+		mcd = gcd(@n*other.n,@d*other.d)		#Se halla el maximo comun divisor entre numerador y denominador
+												#para reducirlos a la minima expresion
+		Fraccion.new((@n*other.n)/mcd,(@d*other.d)/mcd)
+	end
+	
+	def division (other) #Devuelve un nuevo racional que divide al objeto que invoca el que le pasan como parametro
+		mcd = gcd(@n*other.d,@d*other.n)		#Se halla el maximo comun divisor entre numerador y denominador
+												#para reducirlos a la minima expresion
+		Fraccion.new((@n*other.d)/mcd,(@d*other.n)/mcd)
+	end
 end
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,24 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+	def test_suma
+		assert_equal("2/5",Fraccion.new(1,5).suma(Fraccion.new(1,5)).to_s)
+		assert_equal("1/2",Fraccion.new(1,4).suma(Fraccion.new(1,4)).to_s)
+		assert_equal("7/10",Fraccion.new(1,5).suma(Fraccion.new(2,4)).to_s)
+	end
+	
+	def test_resta
+		assert_equal("2/5",Fraccion.new(3,5).resta(Fraccion.new(1,5)).to_s)
+		assert_equal("5/6",Fraccion.new(4,3).resta(Fraccion.new(1,2)).to_s)
+	end
+	
+	def test_producto
+		assert_equal("1/6",Fraccion.new(1,4).producto(Fraccion.new(2,3)).to_s)
+		assert_equal("10/9",Fraccion.new(5,3).producto(Fraccion.new(2,3)).to_s)
+	end
+	
+	def test_division
+		assert_equal("32/3",Fraccion.new(4,3).division(Fraccion.new(1,8)).to_s)
+		assert_equal("7/6",Fraccion.new(1,3).division(Fraccion.new(2,7)).to_s)
+	end
 end
